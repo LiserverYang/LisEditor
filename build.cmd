73fb0e4ed5cbab859aece4b30f7b6c874d7adec7
LiserverYang <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>
2024-02-11 19:29:04 +0800

    Add build

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-g++ .\editor.cpp .\renderer\renderer.cpp -std=c++11 -Werror -O2 -o liseditor
+g++ .\editor.cpp .\renderer\renderer.cpp -std=c++11 -Werror -O2 -o .\build\liseditor
 
 @echo build done
